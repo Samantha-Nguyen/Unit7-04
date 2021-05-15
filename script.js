d2c21af91c5f427b6c40aeb81d821e891a1bf883
SamanthaNguyen1 <>
2021-05-15 15:57:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// This links the button to the "check" function
+document.getElementById('check').addEventListener('click', check)
+
+// set everything to 0
+let age = 0
+let weekday = 0
+
+// this is what checks if you can get the discount
+function check () {
+  age = document.getElementById('age').value
+  weekday = document.getElementById('weekday').value
+  if ((weekday === 'Tuesday' || weekday === 'Thursday') || (age >= 12 && age <= 21)) {
+    document.getElementById('answer').innerHTML = ('You are eligible for a discount at the museum.')
+  } else {
+    document.getElementById('answer').innerHTML = ('Sorry, you are not eligible for a discount.')
+  }
+}
